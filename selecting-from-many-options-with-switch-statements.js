a5b7d2e6e07e73939f1f39d09e53f7066c59985f
AdithyaKeshavT <adityakeshav.ak@gmail.com>
2021-11-24 18:12:39 +0530

    commit

---
+++ selecting-from-many-options-with-switch-statements.js
@@ -0,0 +1,25 @@
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+  case 1:
+    answer = "alpha";
+    return answer;
+    break;
+  case 2:
+    answer = "beta";
+    return answer;
+    break;
+  case 3:
+    answer = "gamma";
+    return answer;
+    break;
+  case 4:
+    answer = "delta";
+    return answer;
+    break;
+  // Only change code above this line
+  }
+}
+
+caseInSwitch(1);
